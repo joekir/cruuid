1a57f8b59fa3e143db7c0ea157eb62c291d13b93
joekir <mail@josephkirwin.com>
2020-10-08 23:24:20 -0700

    add encrypt/decrypt functions

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # cruuid
+
+_en**cr**ypted **uuid**_
+
+A random little script that can encrypt small amounts of data into a UUIDv4 format.
+This is more fiddlesome than might first appear due to blocksize limitation of
+"_good_" symmetric ciphers.
+
+The approach was to use [Triple-DES](https://en.wikipedia.org/wiki/Triple_DES)
+as it's blocksize is only 64 bits, using [CFB mode](https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher_feedback_(CFB))
+avoids the need to pad to the blocksize to also save space.
+
+Finally a truncated (variable based on ciphertext length) HMAC_SHA256 of the data
+is computed to provide some integrity to this (pretty weak) ciphersuite selection.
+
+### Usage
+
+```bash
+$ pipenv install
+$ pipenv shell
+
+(cruuid) bash$ ./cruuid.py encrypt -d foo
+
+Please save this composite encryption key:
+d023a20bc2e97c072aae61640d9252ae265ef1541f8abf52520b9f6095eb54b96ea6a8b7e057ba7a577a02d09c0c04b0983cfea567ec392a
+
+UUIDv4: 489c0e42-05c3-4e7a-6942-93605c70e031
+
+(cruuid) bash$ ./cruuid.py decrypt -u 489c0e42-05c3-4e7a-6942-93605c70e031 -k d023a20bc2e97c072aae61640d9252ae265ef1541f8abf52520b9f6095eb54b96ea6a8b7e057ba7a577a02d09c0c04b0983cfea567ec392a
+b'foo'
+```
--- cruuid.py
+++ cruuid.py
@@ -5,19 +5,27 @@ from Crypto.Cipher import DES3
 from Crypto.Random import get_random_bytes
 from Crypto.Hash import HMAC, SHA256
 
-UUIDV4_PATTERN = re.compile(r'^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
+# UUIDV4_PATTERN = re.compile(r'^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
+UUIDV4_PATTERN_LOOSE = re.compile(r'^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[a-f0-9]{4}-?[a-f0-9]{12}\Z', re.I)
+
 ENCRYPT_SUBPARSER = 'encrypt'
 DECRYPT_SUBPARSER = 'decrypt'
 MIN_MAC_LENGTH = 6
 
-# UUIDify(3DES-CFB(key0, iv, data) || truncated(HMAC(key1, data)))
-# https://tools.ietf.org/html/rfc4122#section-4.1
-
 def encrypt(data):
+    """
+        https://tools.ietf.org/html/rfc4122#section-4.1
+
+        returns UUIDv4 packed in the order of
+        ciphertext+mac[0:12] || V4CONST || cipherext+mac[12:] || iv
+    """
+
     key0 = DES3.adjust_key_parity(get_random_bytes(24))
     key1 = get_random_bytes(32) 
+    composite_key = key0.hex() + key1.hex()
+
     cipher = DES3.new(key0, DES3.MODE_CFB)
-    ciphertext = cipher.encrypt(data.encode())
+    ciphertext = cipher.encrypt(data.encode('utf-8'))
     
     mac = HMAC.new(key1, digestmod=SHA256)
     mac.update(data.encode())
@@ -29,16 +37,38 @@ def encrypt(data):
     truncated_mac_hex = mac.hexdigest()[0:available_mac_length]
     iv_hex = cipher.iv.hex()
     ct_hex = ciphertext.hex()
+    pre_const = ct_hex + truncated_mac_hex
+    enc_uuid = f'{pre_const[0:8]}-{pre_const[8:12]}-4{pre_const[12:]}-{iv_hex[0:4]}-{iv_hex[4:]}'
 
-    result = f'iv: {iv_hex} ({len(iv_hex)})\nciphertext: {ct_hex} ({len(ct_hex)})\nmac: {truncated_mac_hex} ({len(truncated_mac_hex)})\n'
-    print(f'Please save this composite encryption key:\n{key0.hex()}{key1.hex()}')
-    print()
-    print(result)
+    if not UUIDV4_PATTERN_LOOSE.match(enc_uuid):
+        raise ValueError("invalid UUIDv4 created")
 
-def decrypt(uuid, key, iv):
-    print(uuid,key,iv)
+    return enc_uuid, composite_key
 
-def uuidv4(arg_value, pat=UUIDV4_PATTERN):
+def decrypt(enc_uuid, key):
+    """
+        enc_uuid format is
+        ciphertext+mac[0:11] || V4CONST || cipherext+mac[11:] || iv
+    """
+    enc_uuid = enc_uuid.replace('-','')
+    ct_mac = enc_uuid[0:12] + enc_uuid[13:16]
+    iv_bytes = bytes.fromhex(enc_uuid[16:])
+    ct_bytes = bytes.fromhex(ct_mac[0:6])
+    des_key = bytes.fromhex(key[0:48])
+    mac_key = bytes.fromhex(key[48:])
+
+    cipher = DES3.new(des_key, DES3.MODE_CFB, iv=iv_bytes)
+    plaintext_bytes = cipher.decrypt(ct_bytes)
+
+    mac = HMAC.new(mac_key, digestmod=SHA256).update(plaintext_bytes).hexdigest()
+
+    if ct_mac[6:] != mac[0:9]:
+        raise ValueError('Invalid MAC')
+
+    return plaintext_bytes
+
+
+def uuidv4(arg_value, pat=UUIDV4_PATTERN_LOOSE):
     if not pat.match(arg_value):
         raise argparse.ArgumentTypeError('Invalid UUIDv4 input')
     return arg_value
@@ -52,11 +82,13 @@ encrypt_parser.add_argument('-d', '--data', type=str, required=True)
 decrypt_parser = subparsers.add_parser(DECRYPT_SUBPARSER)
 decrypt_parser.add_argument('-u', '--uuidv4', type=uuidv4, required=True)
 decrypt_parser.add_argument('-k', '--key', type=str, required=True)
-decrypt_parser.add_argument('-iv', '--initialization-vector', type=str, required=True)
 
 args = parser.parse_args()
 
 if ENCRYPT_SUBPARSER == args.subparser:
-    encrypt(args.data)
+    enc_uuid, composite_key = encrypt(args.data)
+
+    print(f'Please save this composite encryption key:\n{composite_key}\n')
+    print(f'UUIDv4: {enc_uuid}')
 if DECRYPT_SUBPARSER == args.subparser:
-    decrypt(args.uuid, args.key, args.iv)
+    print(decrypt(args.uuidv4, args.key))
